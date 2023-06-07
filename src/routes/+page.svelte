88c200c286d57173321286b360a75b200ac9b134
Swastik Sharma <swastkk@gmail.com>
2023-06-07 15:46:40 +0530

    Svelte Files removed
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/routes/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1>Hello Welcome to New ISTE site!</h1>
-<p>This is the start and will make this website within 2days :") </p>
-<p>Visit <a href="https://istenith.com">istenith.com</a> use the current website</p>
